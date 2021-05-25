8bf901e00b8f53c7824012ed2276e753a8f0accf
Arno Troch <arno.troch@hotmail.com>
2021-05-25 18:58:04 +0200

    1ste commit van versie 2 van de applicatie, DAL-Laag, Partials, DataOperations functie zoeken op naam

---
+++ MagicTheGathering_DAL/DatabaseOperations.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+
+namespace MagicTheGathering_DAL
+{
+    public static class DatabaseOperations
+    {
+        public static List<Rarity> OphalenRarity()
+        {
+            using (MagicTheGatheringEntities entities = new MagicTheGatheringEntities())
+            {
+                var query = entities.Rarity;
+                return query.ToList();
+            }
+        }
+        public static List<Type> OphalenType()
+        {
+            using (MagicTheGatheringEntities entities = new MagicTheGatheringEntities())
+            {
+                var query = entities.Type;
+                return query.ToList();
+            }
+        }
+        public static List<Color> OphalenColor()
+        {
+            using (MagicTheGatheringEntities entities = new MagicTheGatheringEntities())
+            {
+                var query = entities.Color;
+                return query.ToList();
+            }
+        }
+        public static List<Expansion> OphalenExpansion()
+        {
+            using (MagicTheGatheringEntities entities = new MagicTheGatheringEntities())
+            {
+                var query = entities.Expansion;
+                return query.ToList();
+            }
+        }
+        public static List<Kaart> OphalenKaart()
+        {
+            using (MagicTheGatheringEntities entities = new MagicTheGatheringEntities())
+            {
+                var query = entities.Kaart
+                            .Include(x => x.Type)
+                            .Include(x => x.Color)
+                            .Include(x => x.Rarity)
+                            .Include(x => x.Expansion);
+
+
+
+                return query.ToList();
+            }
+        }
+        public static List<Kaart> OphalenKaartViaZoeken(string naam)
+        {
+            using (MagicTheGatheringEntities entities = new MagicTheGatheringEntities())
+            {
+                var query = entities.Kaart
+                            .Include(x => x.Type)
+                            .Include(x => x.Color)
+                            .Include(x => x.Rarity)
+                            .Include(x => x.Expansion)
+                            .Where(x => x.Naam.Contains(naam));
+                return query.ToList();
+            }
+        }
+        public static List<Kaart> ZoekenOpExpansion(string expansion)
+        {
+            using (MagicTheGatheringEntities entities = new MagicTheGatheringEntities())
+            {
+                var query = entities.Kaart
+                            .Include(x => x.Type)
+                            .Include(x => x.Color)
+                            .Include(x => x.Rarity)
+                            .Include(x => x.Expansion)
+                            .Where(x => x.Expansion.Naam == expansion);
+                return query.ToList();
+            }
+        }
+    }
+}
---
+++ ProjectDataManipulatie/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using MagicTheGathering_DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ProjectDataManipulatie
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            cmbRarity.ItemsSource = DatabaseOperations.OphalenRarity();
+            cmbType.ItemsSource = DatabaseOperations.OphalenType();
+            cmbColor.ItemsSource = DatabaseOperations.OphalenColor();
+            cmbExpansion.ItemsSource = DatabaseOperations.OphalenExpansion();
+        }
+
+        private void btnSearch_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtSearch.Text))
+            {
+                dataMTG.ItemsSource = DatabaseOperations.OphalenKaart();
+                
+            }
+            else
+            {
+                dataMTG.ItemsSource = DatabaseOperations.OphalenKaartViaZoeken(txtSearch.Text);
+            }
+
+            
+            
+
+
+
+
+        }
+
+            
+         
+    }
+    
+}
+
